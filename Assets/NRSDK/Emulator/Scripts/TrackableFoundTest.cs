fafc38b5b8c9622ec7fd0a51b8037e567262e6e6
h-nishihata <nishihata0301@gmail.com>
2020-01-09 22:49:38 +0900

    initially added

---
+++ Assets/NRSDK/Emulator/Scripts/TrackableFoundTest.cs
@@ -0,0 +1,63 @@
+﻿using NRKernal;
+using UnityEngine;
+
+public class TrackableFoundTest : MonoBehaviour
+{
+    public TrackableObserver Observer;
+    public GameObject Obj;
+    void Start()
+    {
+#if !UNITY_EDITOR
+        Destroy(GameObject.Find("EmulatorRoom"));
+#endif
+        Obj.SetActive(false);
+        Observer.FoundEvent += Found;
+        Observer.LostEvnet += Lost;
+    }
+
+    private void Update()
+    {
+        if (NRInput.GetButtonDown(ControllerButton.TRIGGER))
+            Obj.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    private void Found(Vector3 pos, Quaternion qua)
+    {
+        Obj.transform.position = -pos + new Vector3(0, +0.125f, 0);
+        Obj.transform.rotation = Quaternion.Inverse(qua);
+        Obj.SetActive(true);
+    }
+    private void Lost()
+    {
+        Obj.SetActive(false);
+    }
+}
+/*
+using UnityEngine;
+
+public class TrackableFoundTest : MonoBehaviour
+{
+    public TrackableObserver Observer;
+    public GameObject Obj;
+    void Start()
+    {
+#if !UNITY_EDITOR
+        Destroy(GameObject.Find("EmulatorRoom"));
+#endif
+        Obj.SetActive(false);
+        Observer.FoundEvent += Found;
+        Observer.LostEvnet += Lost;
+    }
+
+    private void Found(Vector3 pos, Quaternion qua)
+    {
+        Obj.transform.position = pos;
+        Obj.transform.rotation = qua;
+        Obj.SetActive(true);
+    }
+    private void Lost()
+    {
+        Obj.SetActive(false);
+    }
+}
+*/
